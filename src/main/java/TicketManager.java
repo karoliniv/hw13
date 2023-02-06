838edb932c65af7173bb4f3b7ef9443f1d492e86
Ekaterina Ivanova <parkphuong0@gmail.com>
2023-02-06 03:37:41 +0300

    add code 13.2

--- src/main/java/TicketManager.java
+++ src/main/java/TicketManager.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository repository;
@@ -28,5 +29,21 @@ public class TicketManager {
             return true;
         }
         return false;
+    }
+
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (matches(ticket, from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[result.length] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
+        return result;
     }
 }
